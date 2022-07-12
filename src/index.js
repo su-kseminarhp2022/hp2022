073a61cd3daa1760e09e032af154ce996990b2f2
H-Tatsuhiro <admin@h-tatsu.com>
2022-07-13 07:50:33 +0900

    Update project.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-    document.getElementById('main')
+    document.getElementById('root')
 );
 
 const elem = (
