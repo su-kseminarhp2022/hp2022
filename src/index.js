04cf5bc2b1c922b763a5d25ca18b6f649e1ff387
H.Tatsuhiro <admin@h-tatsu.com>
2022-09-14 13:46:16 +0900

    Enable GitHubPages.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(
 
 const elem = (
     <React.StrictMode>
-        <App />
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
 
