a3e82194781cc85088dd3e853a24884126586138
H-Tatsuhiro <admin@h-tatsu.com>
2022-09-23 19:40:53 +0900

    Setting for GitHub pages.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider} from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const theme = createTheme({
     palette: {
@@ -14,7 +14,7 @@ const theme = createTheme({
 export default function FooterBar() {
     return (
         <ThemeProvider theme={theme}>
-            <Box bgcolor="primary.main" py={5}>
+            <Box sx={{}} bgcolor="primary.main" py={5}>
                 <Typography align="center">
                     Copyright &copy; Shoin University - KIM seminar 2022.
                 </Typography>
