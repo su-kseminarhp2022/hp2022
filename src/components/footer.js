fb0b5c8f57a1dbe890508943d10625ed079e234e
H-Tatsuhiro <admin@h-tatsu.com>
2022-09-25 14:34:57 +0900

    Fixed footer component.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import Box from '@mui/material/Box';
 import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#e3e8c3',
-        },
-    },
-});
 
 export default function FooterBar() {
     return (
-        <ThemeProvider theme={theme}>
-            <Box sx={{}} bgcolor="primary.main" py={5}>
-                <Typography align="center">
-                    Copyright &copy; Shoin University - KIM seminar 2022.
-                </Typography>
-            </Box>
-        </ThemeProvider>
+        <div style={{
+            background: "#e3e8c3",
+            padding: "30px"
+        }}>
+            <Typography align="center">
+                Copyright &copy; Shoin University - KIM seminar 2022.
+            </Typography>
+        </div>
     );
 }
